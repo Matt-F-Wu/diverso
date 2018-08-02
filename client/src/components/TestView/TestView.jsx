d25596f92a967cc1e7103534e9472870969b2996
Hao Wu <wuhao20@stanford.edu>
2018-08-02 01:04:38 -0700

    update heroku config

--- client/src/components/TestView/TestView.jsx
+++ client/src/components/TestView/TestView.jsx
@@ -5,7 +5,7 @@ import React { Component } from 'react';
 
 export default class TestView extends Component {
   render() {
-    <div>I see! For this goal I want to recommond theory and practice related to <span style="font">Symbolism</span></div>
+    <div>I see! For this goal I want to recommond theory and practice related to <span style="font-family: 'Margarine';font-size: 22px;">Symbolism</span></div>
 
   }
 }
