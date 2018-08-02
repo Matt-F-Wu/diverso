a26babe2fdd4d9a49a55bb6ccfde77ba1dea6584
Hao Wu <wuhao20@stanford.edu>
2018-08-02 00:59:01 -0700

    update heroku config

--- starter_data.js
+++ starter_data.js
@@ -1,7 +1,7 @@
 /* 
   This allows you to populate some starter data
 */
-var mongoURL = server_mode === DEPLOY_MODE ? 'mongodb://heroku_06h81ms8:asd123OK@ds163781.mlab.com:63781/heroku_06h81ms8' : 'mongodb://localhost/diverso';
+var mongoURL = process.env.HEROKU ? 'mongodb://heroku_06h81ms8:dflgio6jh53udefg28af0dfnhb@ds163781.mlab.com:63781/heroku_06h81ms8' : 'mongodb://localhost/diverso';
 var mongoose = require('mongoose');
 mongoose.connect(mongoURL);
 
